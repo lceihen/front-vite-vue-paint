e4f16249a3a49c1edc69fd7701be96639522b7bf
chen.lei <chen.lei@qunar.com>
2023-05-07 16:13:15 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,7 @@
 <script setup lang="ts">
-console.log('1')
+import Layout from '@/pages/Layout/index.vue'
 </script>
 
-<template>
-	<div>myapp</div>
-	<div class="text-5xl font-extrabold ...">
-		<span
-			class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500"
-		>
-			Hello world
-		</span>
-	</div>
-</template>
+<template><Layout></Layout></template>
 
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
 	server: {
 		open: true
+	},
+	resolve: {
+		alias: [
+			{
+				find: '@',
+				replacement: path.resolve('./src')
+			}
+		]
 	}
 })
