16b492748d2b402014aea1543fc5098466059f55
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-07-24 18:07:13 +0800

    test: CICD1

--- depoly/notification.sh
+++ depoly/notification.sh
@@ -1,6 +1,7 @@
 WEB_HOOK_URL="https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854"
 current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
 SELF_DOMAIN_NAME=''
+MESSAGE=$CI_COMMIT_MESSAGE
 
 if [[ "$CI_COMMIT_REF_NAME" == "master" ]] || [[ "$CI_COMMIT_REF_NAME" == "main" ]]; then SELF_DOMAIN_NAME=''; else SELF_DOMAIN_NAME=-$CI_COMMIT_REF_NAME; fi
 
@@ -26,7 +27,7 @@ context="{
             },
                {
               \"tag\": \"text\",
-              \"text\": \"发布时间：$CI_COMMIT_MESSAGE\"
+              \"text\": \"发布信息：$MESSAGE\"
             },
             {
               \"tag\": \"a\",
