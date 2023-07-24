021bed8d5004bc5a82c44b33743c8b8b3d211e0d
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-07-24 17:32:51 +0800

    test: CICD1

--- depoly/notification.sh
+++ depoly/notification.sh
@@ -3,9 +3,28 @@ current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
 curl -X POST \
     "https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854" \
     -H 'Content-Type: application/json' \
-    -d '{
-    "msg_type": "text",
-    "content": {
-        "text":"test",
+    -d '
+    {
+  "msg_type": "post",
+  "content": {
+    "post": {
+      "zh-CN": {
+        "title": "CICD消息",
+        "content": [
+          [
+            {
+              "tag": "text",
+              "text": "'$CI_COMMIT_REF_NAME'分支于'$current_datetime'发布上线"
+            },
+            {
+              "tag": "a",
+              "text": "点击查看",
+              "href": "'$CI_COMMIT_REF_NAME'-'canvas${SELF_DOMAIN_NAME}.abclive.cloud'"
+            }
+          ]
+        ]
+      }
     }
-}'
+  }
+}
+    '
