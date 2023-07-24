51ff52d5b6b8bfbd335154159c5867178560dab3
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-07-24 17:38:16 +0800

    test: CICD1

--- depoly/notification.sh
+++ depoly/notification.sh
@@ -1,6 +1,7 @@
 WEB_HOOK_URL="https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854"
 current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
 href="${CI_COMMIT_REF_NAME}-canvas${SELF_DOMAIN_NAME}.abclive.cloud"
+
 curl -X POST \
     "https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854" \
     -H 'Content-Type: application/json' \
@@ -15,12 +16,12 @@ curl -X POST \
           [
             {
               "tag": "text",
-              "text": "'$CI_COMMIT_REF_NAME'分支于'$current_datetime'发布上线"
+              "text": "'CI_COMMIT_REF_NAME'分支于'current_datetime'发布上线"
             },
             {
               "tag": "a",
               "text": "点击查看",
-              "href": "'$href'"
+              "href": "'href'"
             }
           ]
         ]
@@ -28,3 +29,30 @@ curl -X POST \
     }
 }
 }'
+# curl -X POST \
+#     "https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854" \
+#     -H 'Content-Type: application/json' \
+#     -d '
+#     {
+#   "msg_type": "post",
+#   "content": {
+#     "post": {
+#       "zh-CN": {
+#         "title": "CICD消息",
+#         "content": [
+#           [
+#             {
+#               "tag": "text",
+#               "text": "'$CI_COMMIT_REF_NAME'分支于'$current_datetime'发布上线"
+#             },
+#             {
+#               "tag": "a",
+#               "text": "点击查看",
+#               "href": "'$href'"
+#             }
+#           ]
+#         ]
+#       }
+#     }
+# }
+# }'
