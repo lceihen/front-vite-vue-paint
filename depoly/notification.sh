f1af8e9b0c22a0f1a9cf1e6790a92abf4cc23ba3
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-07-24 17:45:49 +0800

    test: CICD1

--- depoly/notification.sh
+++ depoly/notification.sh
@@ -1,41 +1,7 @@
 WEB_HOOK_URL="https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854"
 current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
 href="${CI_COMMIT_REF_NAME}-canvas${SELF_DOMAIN_NAME}.abclive.cloud"
-content="{
-  "msg_type": "post",
-  "content": {
-    "post": {
-      "zh-CN": {
-        "title": "CICD消息",
-        "content": [
-          [
-            {
-              "tag": "text",
-              "text": "CI_COMMIT_REF_NAME分支于current_datetime发布上线"
-            },
-            {
-              "tag": "a",
-              "text": "点击查看",
-              "href": "'href'"
-            }
-          ]
-        ]
-      }
-    }
-}
-}"
-
-echo $content
-
-curl -X POST \
-    "https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854" \
-    -H 'Content-Type: application/json' \
-    -d content
-# curl -X POST \
-#     "https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854" \
-#     -H 'Content-Type: application/json' \
-#     -d '
-#     {
+# content="{
 #   "msg_type": "post",
 #   "content": {
 #     "post": {
@@ -45,16 +11,50 @@ curl -X POST \
 #           [
 #             {
 #               "tag": "text",
-#               "text": "'$CI_COMMIT_REF_NAME'分支于'$current_datetime'发布上线"
+#               "text": "CI_COMMIT_REF_NAME分支于current_datetime发布上线"
 #             },
 #             {
 #               "tag": "a",
 #               "text": "点击查看",
-#               "href": "'$href'"
+#               "href": "'href'"
 #             }
 #           ]
 #         ]
 #       }
 #     }
 # }
-# }'
+# }"
+
+# echo $content
+
+curl -X POST \
+    "https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854" \
+    -H 'Content-Type: application/json' \
+    -d content
+curl -X POST \
+    "https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854" \
+    -H 'Content-Type: application/json' \
+    -d '
+    {
+  "msg_type": "post",
+  "content": {
+    "post": {
+      "zh-CN": {
+        "title": "CICD消息",
+        "content": [
+          [
+            {
+              "tag": "text",
+              "text": "'$CI_COMMIT_REF_NAME'分支于'$current_datetime'发布上线"
+            },
+            {
+              "tag": "a",
+              "text": "点击查看",
+              "href": "'$href'"
+            }
+          ]
+        ]
+      }
+    }
+}
+}'
