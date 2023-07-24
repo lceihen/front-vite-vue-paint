50ed626634ee8072ad025ae79153f353f042baa0
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-07-24 17:53:25 +0800

    test: CICD1

--- depoly/notification.sh
+++ depoly/notification.sh
@@ -1,7 +1,11 @@
 WEB_HOOK_URL="https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854"
 current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
 href="${CI_COMMIT_REF_NAME}-canvas${SELF_DOMAIN_NAME}.abclive.cloud"
-context="{
+
+curl -X POST \
+    $WEB_HOOK_URL \
+    -H 'Content-Type: application/json' \
+    -d "{
   \"msg_type\": \"post\",
   \"content\": {
     \"post\": {
@@ -24,8 +28,3 @@ context="{
     }
   }
 }"
-
-curl -X POST \
-    $WEB_HOOK_URL \
-    -H 'Content-Type: application/json' \
-    -d $context
