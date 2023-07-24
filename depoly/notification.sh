e1cec92e30a9799586ff37d6ebe37088d5019e0a
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-07-24 17:47:08 +0800

    test: CICD1

--- depoly/notification.sh
+++ depoly/notification.sh
@@ -35,24 +35,25 @@ curl -X POST \
     $WEB_HOOK_URL \
     -H 'Content-Type: application/json' \
     -d '{
-    "msg_type": "text",
-    "content": {
-        "post": {
-            "zh_cn": {
-                "title": "CICD发布",
-                "content": [
-                    [
-                        {
-                            "tag": "text",
-                            "text": "'$CI_COMMIT_REF_NAME'",
-                            "url": "'canvas${SELF_DOMAIN_NAME}.abclive.cloud'"
-                        }
-                    ],
-                    [
-
-                    ]
-                ]
+  "msg_type": "post",
+  "content": {
+    "post": {
+      "zh-CN": {
+        "title": "富文本消息测试！",
+        "content": [
+          [
+            {
+              "tag": "text",
+              "text": "你的小可爱上线了！"
+            },
+            {
+              "tag": "a",
+              "text": "点击查看",
+              "href": "https://sspai.com/u/100gle/updates"
             }
-        }
+          ]
+        ]
+      }
     }
+  }
 }'
