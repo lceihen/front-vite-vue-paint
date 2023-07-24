3a99838df3300e20782e95a14bedf1c83b6c5216
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-07-24 16:02:56 +0800

    test: notification

---
+++ depoly/notification.sh
@@ -0,0 +1,25 @@
+WEB_HOOK_URL: "https://open.feishu.cn/open-apis/bot/v2/hook/f0b1358b-38ce-4f01-b629-64c0d2fbe854"
+curl -X POST \
+    $WEB_HOOK_URL \
+    -H 'Content-Type: application/json' \
+    -d '{
+    "msg_type": "post",
+    "content": {
+        "post": {
+            "zh_cn": {
+                "title": "WGCLOUD告警",
+                "content": [
+                    [
+                        {
+                            "tag": "text",
+                            "text": "'$content'"
+                        }
+                    ],
+                    [
+
+                    ]
+                ]
+            }
+        }
+    }
+}'
