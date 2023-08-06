f7725a8e537e80276ed39d311e6abc2ef31a33e4
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-08-06 08:54:29 +0800

    fix: 修复version的生成

--- depoly/version.sh
+++ depoly/version.sh
@@ -2,33 +2,35 @@
 
 file_name='../public/version'
 
-pwd
-
-ls
-
 touch $file_name
 
-line_count=$(wc -l <"$file_name")
+# pwd
 
-log="$(git log -5 --pretty=format:"%h**%s**%cd |" --date=format:"%Y年%m月%d日%H时%M分%S秒")"
+# ls
 
-echo $log
+# touch $file_name
 
-handleWriteLog() {
+# line_count=$(wc -l <"$file_name")
 
-    echo "$log" >"${file_name}.tmp"
-    cat "$file_name" >>"$file_name.tmp"
-    mv "$file_name.tmp" "$file_name"
-    if [ "$line_count" -ge 5 ]; then
-        head -n 5 "$file_name" >"$file_name.tmp"
-        mv "$file_name.tmp" "$file_name"
-    fi
-    return 0
-}
+# log="$(git log -5 --pretty=format:"%h**%s**%cd |" --date=format:"%Y年%m月%d日%H时%M分%S秒")"
 
-if handleWriteLog; then
-    echo ""
-else
-    echo "命令执行失败，进行错误处理"
-    exit 1
-fi
+# echo $log
+
+# handleWriteLog() {
+
+#     echo "$log" >"${file_name}.tmp"
+#     cat "$file_name" >>"$file_name.tmp"
+#     mv "$file_name.tmp" "$file_name"
+#     if [ "$line_count" -ge 5 ]; then
+#         head -n 5 "$file_name" >"$file_name.tmp"
+#         mv "$file_name.tmp" "$file_name"
+#     fi
+#     return 0
+# }
+
+# if handleWriteLog; then
+#     echo ""
+# else
+#     echo "命令执行失败，进行错误处理"
+#     exit 1
+# fi
