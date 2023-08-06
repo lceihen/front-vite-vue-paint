e8e4b97f0edb6bb4f9f9b52e57b1a138957ecd99
lceihenX <132864726+lceihenX@users.noreply.github.com>
2023-08-06 09:02:32 +0800

    fix: 修复路径

--- depoly/version.sh
+++ depoly/version.sh
@@ -8,33 +8,27 @@ pwd
 
 touch $file_name
 
-# pwd
+line_count=$(wc -l <"$file_name")
 
-# ls
+log="$(git log -5 --pretty=format:"%h**%s**%cd |" --date=format:"%Y年%m月%d日%H时%M分%S秒")"
 
-# touch $file_name
+echo $log
 
-# line_count=$(wc -l <"$file_name")
+handleWriteLog() {
 
-# log="$(git log -5 --pretty=format:"%h**%s**%cd |" --date=format:"%Y年%m月%d日%H时%M分%S秒")"
+    echo "$log" >"${file_name}.tmp"
+    cat "$file_name" >>"$file_name.tmp"
+    mv "$file_name.tmp" "$file_name"
+    if [ "$line_count" -ge 5 ]; then
+        head -n 5 "$file_name" >"$file_name.tmp"
+        mv "$file_name.tmp" "$file_name"
+    fi
+    return 0
+}
 
-# echo $log
-
-# handleWriteLog() {
-
-#     echo "$log" >"${file_name}.tmp"
-#     cat "$file_name" >>"$file_name.tmp"
-#     mv "$file_name.tmp" "$file_name"
-#     if [ "$line_count" -ge 5 ]; then
-#         head -n 5 "$file_name" >"$file_name.tmp"
-#         mv "$file_name.tmp" "$file_name"
-#     fi
-#     return 0
-# }
-
-# if handleWriteLog; then
-#     echo ""
-# else
-#     echo "命令执行失败，进行错误处理"
-#     exit 1
-# fi
+if handleWriteLog; then
+    echo ""
+else
+    echo "命令执行失败，进行错误处理"
+    exit 1
+fi
